71eb468b7dc53eff72fe1afc93ea7a215f30d85a
Justin Wendlandt <juswen@microsoft.com>
2020-04-06 22:38:13 -0700

    Initial Commit

---
+++ CreateServicePrincipal/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CreateServicePrincipal
+{
+    class Program
+    {
+        private static readonly string ClientSideClientId = "";
+        private static readonly string ClientSideTenantId = "";
+        private static readonly string ServerSideClientId = "";
+        private static readonly string ServerSideTenantId = "";
+        private static readonly string ServerSideClientSecret = "";
+
+        static async Task Main(string[] args)
+        {
+            var publicApplicationClientBuilder = PublicClientApplicationBuilder
+                .Create(ClientSideClientId)
+                .WithTenantId(ClientSideTenantId)
+                .WithDefaultRedirectUri()
+                .Build();
+
+            var scopes = new List<string>()
+            {
+                "api://serviceprincipalservice-server/access_as_user",
+            };
+
+            var authenticationResult = await publicApplicationClientBuilder.AcquireTokenWithDeviceCode(scopes, (dcr) =>
+                {
+                    Console.WriteLine(dcr.Message);
+                    return Task.FromResult(0);
+                })
+                .ExecuteAsync();
+
+            await CreateServicePrincipalWithSecurityGroupClaims(authenticationResult.AccessToken, "TestPrincipal");
+        }
+
+        private static async Task CreateServicePrincipalWithSecurityGroupClaims(string userAccessToken, string servicePrincipalName)
+        {
+            var delegateAuthenticationProvider = new DelegateAuthenticationProvider(
+                async (requestMessage) =>
+                {
+                    var authenticationResult = await FetchAccessTokenOnBehalfOf(userAccessToken);
+                    var graphAccessToken = authenticationResult.AccessToken;
+
+                    // Append the access token to the request.
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", graphAccessToken);
+                });
+
+            var application = new Application()
+            {
+                DisplayName = servicePrincipalName,
+                GroupMembershipClaims = "SecurityGroup",
+            };
+
+            var graphServiceClient = new GraphServiceClient(delegateAuthenticationProvider);
+            var createdApplication = await graphServiceClient.Applications
+                .Request()
+                .AddAsync(application);
+        }
+
+        private static async Task<AuthenticationResult> FetchAccessTokenOnBehalfOf(string accessToken)
+        {
+            var confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(ServerSideClientId)
+                .WithClientSecret(ServerSideClientSecret)
+                .WithTenantId(ServerSideTenantId)
+                .Build();
+
+            var scopes = new List<string>()
+            {
+                "https://graph.microsoft.com/Application.ReadWrite.All",
+            };
+
+            var userAssertion = new UserAssertion(accessToken);
+            var graphAccessToken = await confidentialClientApplication.AcquireTokenOnBehalfOf(scopes, userAssertion)
+                .ExecuteAsync();
+
+            return graphAccessToken;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# service-principal-service-example
- An example of using on behalf of flow to access Graph from a service
+# Service Principal Example
+
+This is an example of how to use Microsoft Graph and the On Behalf of Flow in Azure AD to create a service principal.
+
+## Application Registration for Server Application
+
+Steps are leveraging steps from: [Azure AD Docs](https://github.com/Azure-Samples/active-directory-dotnet-native-aspnetcore-v2/tree/master/2.%20Web%20API%20now%20calls%20Microsoft%20Graph#register-the-service-app-todolistservice).
+
+1. Navigate to Application Registration page
+1. Click on Add New Application
+    1. Provide a name for the registration (ServicePrincipalService-Server)
+    1. For the Supported Account Types choose Accounts in this organization directory only
+1. On the overview tab we will need the application id and tenant id
+1. Under Certificates and Secrets, create a secret - we will need this somehow too. Currently our plan is to store it inside Azure Key Vault for the applications to use so it will not be exposed anywhere
+1. Select API Permissions
+    1. Click Add Permission
+        1. Select Microsoft Graph under Microsoft APIs
+        1. Go under Delegated permissions
+            1. Select Application.ReadWrite.All
+    1. Select "Grant admin consent for…" This is required for the on-behalf-of flow because a UI isn't available for users to consent with
+1. Select Expose and API
+    1. Name the app (something like api://wba-rx-virtual-consult-server)
+    1. Enter in a scope name like access_as_user
+    1. Select Admin and Users
+    1. Enter display name and description (anything works like "Access the server as a user"
+    1. Select AddScope
+1. Click on Manifest
+    1. Edit the "accessTokenAcceptedVersion" and set that value to "2"
+
+## Application Registration for Client Application
+
+Steps are leveraging steps from: [Azure AD Docs](https://github.com/Azure-Samples/active-directory-dotnet-native-aspnetcore-v2/tree/master/2.%20Web%20API%20now%20calls%20Microsoft%20Graph#register-the-client-app-todolistclient).
+
+1. Navigate to App Registrations page
+1. Add new Application
+    1. Provide a name for the registration (ServicePrincipalService-Client)
+    1. For the Supported Account Types choose Accounts in this organization directory only
+1. On the overview tab, we will need the application id and tenant id
+1. Select Authentication tab
+    1. Click on Add a platform
+    1. Add the platform that you are using (in our case just add native client application since we are using device code in this repo)
+1. Select API Permissions tab
+    1. Click add permission
+    1. Click My APIs
+    1. Select the server app (wba-rx-virtual-consult-server)
+    1. In delegated permissions select access_as_user
+    1. Select add permission button
+
+Finally, go back to the server application
+
+1. Open up Manifest
+1. Change "knownClientApplications": [] to be
+
+``` json
+"knownClientApplications": [
+         "<client application guid>"
+ ],
+```
